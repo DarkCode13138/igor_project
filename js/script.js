84233f60cbbc5509a0b748acf20e7e6c7ebd0224
Igor <igor.chudesny1991@gmail.com>
2022-09-04 18:46:48 +0200

    Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let age = 29
+let userName = 'Igor';
+console.log(age , userName);
+
+const header =  document.querySelector(".header");
+console.log(header);
+
+const nav__link =  document.querySelectorAll(".nav__link");
+console.log(nav__link);
+
+alert("6" / "2");
+
+let str = "123";
+alert( str)
+
+function helloHomework() {
+    console.log("Hello");
+}
+
+helloHomework();
+
+
+function JavaScript() {
+    console.log("HelloJavaScript")
+
+}
+
+JavaScript();
+
+
+
+
+
